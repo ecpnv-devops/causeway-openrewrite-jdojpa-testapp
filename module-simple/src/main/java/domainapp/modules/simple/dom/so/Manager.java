6f7284961280e2d31938024fa3e5893905924e56
Patrick Deenen <patrick@opencircle.solutions>
2025-02-06 12:04:17 +0100

    Delete unused imports

--- module-simple/src/main/java/domainapp/modules/simple/dom/so/Manager.java
+++ module-simple/src/main/java/domainapp/modules/simple/dom/so/Manager.java
@@ -3,14 +3,10 @@ package domainapp.modules.simple.dom.so;
 import java.util.Set;
 
 import javax.inject.Named;
-import javax.jdo.annotations.DatastoreIdentity;
-import javax.jdo.annotations.IdGeneratorStrategy;
 import javax.jdo.annotations.Inheritance;
 import javax.jdo.annotations.InheritanceStrategy;
 import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.Persistent;
-import javax.jdo.annotations.Version;
-import javax.jdo.annotations.VersionStrategy;
 
 import org.apache.causeway.applib.annotation.Action;
 import org.apache.causeway.applib.annotation.Collection;
